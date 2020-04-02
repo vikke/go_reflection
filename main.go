f244e4a829d1a8ad269868a07ebb9f2d68b86ab5
MATSUNAGA Ichiro <vikke.bsd@gmail.com>
2020-04-02 16:54:50 +0900

    fix.

--- main.go
+++ main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
-	"github.com/lifestyledesign/tmp/hoge"
 	"log"
 	"reflect"
 )
 
+type MyStruct struct {
+}
+
+type ReturnValue struct {
+	val string
+}
+
+func (ms *MyStruct) Method1(i int, s string) (out ReturnValue) {
+	log.Printf("foobar")
+	return ReturnValue{val: s + s}
+}
 func main() {
-	ms := &hoge.MyStruct{}
+	ms := &MyStruct{}
 	receiver := reflect.ValueOf(ms)
 	log.Println("before")
-	receiver.MethodByName("Method1").Call([]reflect.Value{})
-	log.Println("after")
+	receiver.MethodByName("Method1").Call([]reflect.Value{reflect.ValueOf(1), reflect.ValueOf("aa")})
+
 }
--- tmp/hoge/target.go
+++ tmp/hoge/target.go
@@ -1,10 +1,1 @@
-package hoge
 
-import "log"
-
-type MyStruct struct {
-}
-
-func (ms *MyStruct) method1() {
-	log.Printf("foobar")
-}
